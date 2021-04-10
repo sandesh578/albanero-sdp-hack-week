7e45814a0a0f48eb330debff9bb0db3b7476737f
sandesh578 <66244625+sandesh578@users.noreply.github.com>
2021-04-10 07:44:39 +0530

    Add files via upload

---
+++ day1/Ques7.java
@@ -0,0 +1,53 @@
+package day1;
+
+import java.util.Scanner;
+public class Ques7 {
+
+	public static int maxConsecutiveOnes(int arr[],int k)
+	{
+		int n=arr.length;
+		int max=0;
+		int zeroCount=0;
+		int start=0;
+		
+		for(int i=0;i<n;i++)
+		{
+			if(arr[i]==0)
+			{
+				zeroCount++;
+			}
+			while(zeroCount>k)
+			{
+				if(arr[start]==0)
+				{
+					zeroCount--;
+				}
+				start++;
+			}
+			max=Math.max(i-start+1,max);
+		}
+	
+		return max;
+	}
+	public static void main(String[] args) {
+       Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Enter the size of array of 0's and 1's:");
+		int n=scan.nextInt();
+		
+		System.out.println("Enter the number of replacement:");
+		int k=scan.nextInt();
+		
+		int arr[]=new int[n];
+		System.out.println("Enter "+n+" elements to an array:");
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=scan.nextInt();
+		}
+		
+		System.out.println("\n"+maxConsecutiveOnes(arr,k));
+		scan.close();
+
+	}
+
+}
